06862da03f01da54af0658c3a02a61b99afc8333
Jung JaeWoong <jjw_0609@naver.com>
2023-09-06 22:10:36 +0900

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue3 + tailwindcss + firebase
+
+https://jjw0609.github.io/vue3-twitter-clone/
---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 class="text-lg bg-gray-900 text-white ml-10 mt-4 pl-10 pt-3 hover:bg-gray-600 hover:text-red-300">hello world</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
