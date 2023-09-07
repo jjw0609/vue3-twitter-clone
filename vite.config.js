f8e29b575aeb49f52b615766fb52c809dbc064ae
Jung JaeWoong <jjw_0609@naver.com>
2023-09-08 00:10:47 +0900

    Fix: update deploy setting

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig(({command, mode}) => ({
   plugins: [vue()],
-  publicDir: command === 'build' ? '/vue3-twitter-clone/' : '/'
+  publicDir: command === 'build' ? '/vue3-twitter-clone' : '/'
 }));
