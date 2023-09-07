838cec5cd28477b525a5dc0f00b9fb02dc397ee4
Jung JaeWoong <jjw_0609@naver.com>
2023-09-07 23:34:17 +0900

    feat: add sidemenu

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,48 @@
 <template>
-  <h1 class="text-lg bg-gray-900 text-white ml-10 mt-4 pl-10 pt-3 hover:bg-gray-600 hover:text-red-300">hello world</h1>
+  <div class="flex h-screen">
+    <div class="w-1/4 mt-5 ml-10">
+      <!-- twitter logo -->
+      <i class="fa-brands fa-twitter text-2xl text-primary ml-4"></i>
+      <!-- sidemenu icons -->
+      <div class="flex flex-col items-start">
+        <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+          <i class="fas fa-home fa-fw text-2xl"></i>
+          <span class="ml-5 text-lg">홈</span>
+        </div>
+        <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+          <i class="fas fa-hashtag fa-fw text-2xl"></i>
+          <span class="ml-5 text-lg">탐색하기</span>
+        </div>
+        <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+          <i class="far fa-bell fa-fw text-2xl"></i>
+          <span class="ml-5 text-lg">알림</span>
+        </div>
+        <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+          <i class="far fa-envelope fa-fw text-2xl"></i>
+          <span class="ml-5 text-lg">쪽지</span>
+        </div>
+        <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+          <i class="far fa-bookmark fa-fw text-2xl"></i>
+          <span class="ml-5 text-lg">북마크</span>
+        </div>
+        <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+          <i class="far fa-list-alt fa-fw text-2xl"></i>
+          <span class="ml-5 text-lg">리스트</span>
+        </div>
+        <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+          <i class="far fa-user fa-fw text-2xl"></i>
+          <span class="ml-5 text-lg">프로필</span>
+        </div>
+        <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+          <i class="fas fa-ellipsis-h fa-fw text-2xl"></i>
+          <span class="ml-5 text-lg">더보기</span>
+        </div>
+      </div>
+      <!-- tweet button -->
+      <button class="mt-3 bg-primary text-white w-60 h-12 rounded-full hover:bg-dark">Tweet</button>
+    </div>
+    <div class="flex-1 bg-red-200">dd</div>
+  </div>
 </template>
 
 <script>
