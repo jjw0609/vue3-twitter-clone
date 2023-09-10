ff999102a1cd821bbeeeb34d293fc16e5ecc78de
Jung JaeWoong <jjw_0609@naver.com>
2023-09-10 22:16:58 +0900

    fix: modify retweet icon class

--- src/App.vue
+++ src/App.vue
@@ -105,7 +105,7 @@
                     <span class="ml-1 text-gray-500 text-sm">1</span>
                   </div>
                   <div class="text-gray-500 hover:text-green-500">
-                    <i class="far fa-retweet hover:bg-green-50 rounded-full p-2"></i>
+                    <i class="fa fa-retweet hover:bg-green-50 rounded-full p-2"></i>
                     <span class="ml-1 text-gray-500 text-sm">1</span>
                   </div>
                   <div class="text-gray-500 hover:text-red-500">
