3950f81e5889948c7c20d14595c438d0b0b3ddb1
Jung JaeWoong <jjw_0609@naver.com>
2023-09-11 22:40:17 +0900

    fix: 사이드메뉴 여백 조정

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,41 @@
 <template>
-  <div class="flex h-screen">
+  <div class="flex h-screen container mx-auto">
     <!-- side section -->
-    <div class="w-1/4 pt-5 ml-10 flex flex-col justify-between border-r border-gray-100">
+    <div class="w-20 lg:w-1/4 pt-5 lg:ml-10 flex flex-col justify-between border-r border-gray-100">
       <div class="flex flex-col items-center lg:items-start">
         <!-- twitter logo -->
         <i class="fab fa-twitter text-3xl text-primary lg:ml-4 mb-3"></i>
         <!-- sidemenu icons -->
         <div class="flex flex-col items-start space-y-1">
-          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer">
             <i class="fas fa-home fa-fw text-2xl"></i>
             <span class="ml-5 text-lg hidden lg:inline-block">홈</span>
           </div>
-          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer">
             <i class="fas fa-hashtag fa-fw text-2xl"></i>
             <span class="ml-5 text-lg hidden lg:inline-block">탐색하기</span>
           </div>
-          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer">
             <i class="far fa-bell fa-fw text-2xl"></i>
             <span class="ml-5 text-lg hidden lg:inline-block">알림</span>
           </div>
-          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer">
             <i class="far fa-envelope fa-fw text-2xl"></i>
             <span class="ml-5 text-lg hidden lg:inline-block">쪽지</span>
           </div>
-          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer">
             <i class="far fa-bookmark fa-fw text-2xl"></i>
             <span class="ml-5 text-lg hidden lg:inline-block">북마크</span>
           </div>
-          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer">
             <i class="far fa-list-alt fa-fw text-2xl"></i>
             <span class="ml-5 text-lg hidden lg:inline-block">리스트</span>
           </div>
-          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer">
             <i class="far fa-user fa-fw text-2xl"></i>
             <span class="ml-5 text-lg hidden lg:inline-block">프로필</span>
           </div>
-          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer">
             <i class="fas fa-ellipsis-h fa-fw text-2xl"></i>
             <span class="ml-5 text-lg hidden lg:inline-block">더보기</span>
           </div>
@@ -44,7 +44,7 @@
         <div class="w-full lg:pr-3 flex justify-center">
           <button class="mt-3 bg-primary text-white lg:w-full w-12 h-12 rounded-full hover:bg-dark">
             <span class="hidden lg:block">트윗</span>
-            <i class="lg:hidden fas fa-plus"></i>
+            <i class="fas fa-plus lg:hidden"></i>
           </button>
         </div>
       </div>
