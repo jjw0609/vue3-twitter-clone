086fefada85912c94a18dd213dcd9daf10d2618e
Jung JaeWoong <jjw_0609@naver.com>
2023-10-03 23:20:22 +0900

    firebase authentication

---
+++ src/firebase/index.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBsTEvv_k5WEyjnQy0Cdhr5ZNyZe2N00js",
+    authDomain: "twitter-clone-inflearn-10fc8.firebaseapp.com",
+    projectId: "twitter-clone-inflearn-10fc8",
+    storageBucket: "twitter-clone-inflearn-10fc8.appspot.com",
+    messagingSenderId: "325557618636",
+    appId: "1:325557618636:web:fe74e1dad3ca4f98af3f69",
+    measurementId: "G-NT1D3TVGPK"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const db = firebase.firestore();
+export const storage = firebase.storage();
