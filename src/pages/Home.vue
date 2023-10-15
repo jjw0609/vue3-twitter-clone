24b2f52a905fcd27a2ea1663246e868d4767036b
Jung JaeWoong <jjw_0609@naver.com>
2023-10-15 22:59:45 +0900

    feat: add user info on tweet

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -30,7 +30,7 @@ import Trends from '../components/Trends.vue';
 import Tweet from '../components/Tweet.vue';
 import { ref, computed, onBeforeMount } from 'vue';
 import store from '../store';
-import { TWEET_COLLECTION } from '../firebase';
+import {TWEET_COLLECTION, USER_COLLECTION} from '../firebase';
 export default {
   components: { Trends, Tweet },
   setup() {
@@ -40,17 +40,28 @@ export default {
 
     onBeforeMount(() => {
       TWEET_COLLECTION.orderBy('created_at', 'desc').onSnapshot(snapshot => {
-        snapshot.docChanges().forEach(change => {
+        snapshot.docChanges().forEach(async change => {
+          let tweet = await getUserInfo(change.doc.data());
+
           if(change.type === 'added') {
-            tweets.value.splice(change.newIndex, 0, change.doc.data());
+            tweets.value.splice(change.newIndex, 0, tweet);
           } else if(change.type === 'modified') {
-            tweets.value.splice(change.oldIndex, 1, change.doc.data());
+            tweets.value.splice(change.oldIndex, 1, tweet);
           } else if(change.type === 'removed') {
             tweets.value.splice(change.oldIndex, 1);
           }
         })
       })
     })
+
+    const getUserInfo = async (tweet) => {
+      const doc = await USER_COLLECTION.doc(tweet.uid).get();
+      tweet.profile_image_url = doc.data().profile_image_url;
+      tweet.email = doc.data().email;
+      tweet.username = doc.data().username;
+      // tweet = {...tweet, ...doc.data()};
+      return tweet;
+    }
 
     const onAddTweet = async () => {
       try {
