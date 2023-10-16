5a5e8b285dc96a5840b2cb041e8a735992d28e07
Jung JaeWoong <jjw_0609@naver.com>
2023-10-16 23:25:54 +0900

    feat: create commentModal popup

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -1,50 +1,62 @@
 <template>
   <!-- tweets -->
-  <div class="flex px-3 py-3 border-b border-gray-100 hover:bg-gray-50 cursor-pointer">
-    <img :src="currentUser.profile_image_url" class="w-10 h-10 rounded-full hover:opacity-80 cursor-pointer" />
-    <div class="ml-3 flex-1 flex flex-col space-y-1">
-      <div class="text-sm space-x-1">
-        <span class="font-bold">{{ tweet.email }}</span>
-        <span class="text-gray-500 text-xs">@{{ tweet.username }}</span>
+  <div className="flex px-3 py-3 border-b border-gray-100 hover:bg-gray-50 cursor-pointer">
+    <img :src="currentUser.profile_image_url" class="w-10 h-10 rounded-full hover:opacity-80 cursor-pointer"/>
+    <div className="ml-3 flex-1 flex flex-col space-y-1">
+      <div className="text-sm space-x-1">
+        <span className="font-bold">{{ tweet.email }}</span>
+        <span className="text-gray-500 text-xs">@{{ tweet.username }}</span>
         <span>•</span>
-        <span class="text-gray-500 text-xs">{{ moment(tweet.created_at).fromNow() }}</span>
+        <span className="text-gray-500 text-xs">{{ moment(tweet.created_at).fromNow() }}</span>
       </div>
       <!-- tweet body -->
       <div>
         {{ tweet.tweet_body }}
       </div>
       <!-- tweet actions -->
-      <div class="flex justify-between">
-        <div class="text-gray-500 hover:text-primary">
-          <i class="far fa-comment hover:bg-blue-50 rounded-full p-2"></i>
-          <span class="ml-1 text-sm">{{ tweet.num_comments }}</span>
+      <div className="flex justify-between">
+        <!-- comment button -->
+        <div @click="showCommentModal = true" className="text-gray-500 hover:text-primary">
+          <i className="far fa-comment hover:bg-blue-50 rounded-full p-2"></i>
+          <span className="ml-1 text-sm">{{ tweet.num_comments }}</span>
         </div>
-        <div class="text-gray-500 hover:text-green-500">
-          <i class="fa fa-retweet hover:bg-green-50 rounded-full p-2"></i>
-          <span class="ml-1 text-sm">{{ tweet.num_retweets }}</span>
+        <!-- retweet button -->
+        <div className="text-gray-500 hover:text-green-500">
+          <i className="fa fa-retweet hover:bg-green-50 rounded-full p-2"></i>
+          <span className="ml-1 text-sm">{{ tweet.num_retweets }}</span>
         </div>
-        <div class="text-gray-500 hover:text-red-500">
-          <i class="far fa-heart hover:bg-red-50 rounded-full p-2"></i>
-          <span class="ml-1 text-sm">{{ tweet.num_likes }}</span>
+        <!-- like button -->
+        <div className="text-gray-500 hover:text-red-500">
+          <i className="far fa-heart hover:bg-red-50 rounded-full p-2"></i>
+          <span className="ml-1 text-sm">{{ tweet.num_likes }}</span>
         </div>
-        <div class="text-gray-500 hover:text-primary">
-          <i class="far fa-share-square hover:bg-blue-50 rounded-full p-2"></i>
+        <!-- share button -->
+        <div className="text-gray-500 hover:text-primary">
+          <i className="far fa-share-square hover:bg-blue-50 rounded-full p-2"></i>
         </div>
       </div>
     </div>
   </div>
+  <comment-modal v-if="showCommentModal" @close-modal="showCommentModal = false"/>
 </template>
 
 <script>
 import moment from 'moment';
-  export default{
-    props: ['currentUser', 'tweet'],
-    setup() {
-      return {
-        moment
-      }
+import {ref} from 'vue';
+import commentModal from './commentModal.vue';
+
+export default {
+  components: { commentModal },
+  props: ['currentUser', 'tweet'],
+  setup() {
+    const showCommentModal = ref(false);
+
+    return {
+      moment,
+      showCommentModal
     }
   }
+}
 </script>
 
 <style></style>
